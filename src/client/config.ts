fd8a20147f1bde41b4453f457e43e0c8976142c8
Alex Kizer <kizerkizerkizerkizerkizer@gmail.com>
2026-05-05 10:05:52 -0500

    config

--- src/client/config.ts
+++ src/client/config.ts
@@ -1,4 +1,7 @@
-let __WT_PORT: string;
-let __WT_URL: string;
+declare const __WT_PORT: string;
+declare const __WT_URL: string;
 
-export { __WT_PORT as WT_PORT, __WT_URL as WT_URL };
+const WT_PORT = __WT_PORT;
+const WT_URL = __WT_URL;
+
+export { WT_PORT, WT_URL };
